04855a5821439ae082bcc19b828e31e9df75353d
Donald Armstead <donald.armstead@gmail.com>
2018-09-19 00:34:40 -0400

    feat: working file store

--- test_data_api2.0.py
+++ test_data_api2.0.py
@@ -2,8 +2,8 @@ from ohmysportsfeedspy import MySportsFeeds
 from cryptography.fernet import Fernet
 import json
 
-msf = MySportsFeeds(version="2.0", store_type=None)
+msf = MySportsFeeds(version="2.0", store_type='file', store_location='results/')
 
 msf.authenticate("76991493-077a-4a0f-92da-874b8c", "MYSPORTSFEEDS")
-output = msf.msf_get_data(league='mlb',season='2016-playoff',feed='seasonal_games',format='csv')
+output = msf.msf_get_data(league='mlb',season='2016-playoff',feed='seasonal_games',format='json')
 print output
