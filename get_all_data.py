8f4dd873dfca90bf260272e3a2dc5ddab70464bb
Donald Armstead <donald.armstead@gmail.com>
2018-09-19 23:50:32 -0400

    feat: basic encryption added

--- get_all_data.py
+++ get_all_data.py
@@ -2,9 +2,14 @@ from ohmysportsfeedspy import MySportsFeeds
 from cryptography.fernet import Fernet
 import json
 
+with open('key.txt', 'r') as myfile:
+  key = myfile.read()
+f = Fernet(key)
+token = 'gAAAAABboxejr3hkCDwGeNSEQDPjLbw2T-M41JclD-7ZqqhZWAM61cjqybisLMBFdARpk4L0NsX90tpRNabJcr-oP31W--eiqcbbE8Qe5ZOz9273CdNy5Ts='
+
 msf = MySportsFeeds(version="2.0", store_type='file', store_location='results/')
 
-msf.authenticate("76991493-077a-4a0f-92da-874b8c", "MYSPORTSFEEDS")
+msf.authenticate(f.decrypt(token), "MYSPORTSFEEDS")
 #output = msf.msf_get_data(league='mlb',season='2016-playoff',feed='seasonal_games',format='json')
 #print output
 league = 'nfl'
