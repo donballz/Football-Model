5ee2a28855d72734f205ba2ec4ac03a78b76db03
Donald Armstead <donald.armstead@gmail.com>
2018-09-19 23:37:39 -0400

    chore: final ruby test; does not work

--- test_data.rb
+++ test_data.rb
@@ -7,6 +7,6 @@ msf = MySportsFeeds.new(version="1.0", true)
 pwd = "2F6F490793BE248DE3B200CF16C545A0"
 key = File.read("#{PATH}/key.txt")
 
-msf.authenticate("810ff31c-e7ca-4926-a717-0bef77", pwd.decrypt(key))
+msf.authenticate("76991493-077a-4a0f-92da-874b8c", pwd.decrypt(key))
 
 data = msf.msf_get_data('nfl', '2015-2016-regular', 'cumulative_player_stats', 'json', 'team' => 'detroit-lions')
